792f411054e7d15c876f787e25f2188c2824f31f
Phruksa <phruksa4165@gmail.com>
2020-11-26 12:23:44 +0700

    add settting

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from .views import Home,Signup,Login,logout,Cart,Checkout,OrderView
 from .middlewares import LoginCheckMiddleware,LogoutCheckMiddleware
 
 urlpatterns = [
-    path('',Home.as_view(), name='home'),  
+    path('home',Home.as_view(), name='home'),  
     path('signup',LogoutCheckMiddleware(Signup.as_view()), name='signup'),
     path('login',LogoutCheckMiddleware(Login.as_view()), name='login'),
     path('logout',LoginCheckMiddleware(logout), name='logout'),

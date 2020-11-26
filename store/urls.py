25e36ec22785750b596f95860bfed8596025dc8a
Phruksa <phruksa4165@gmail.com>
2020-11-26 18:45:00 +0700

    heroku

--- store/urls.py
+++ store/urls.py
@@ -3,13 +3,13 @@ from .views import Home,Signup,Login,logout,Cart,Checkout,OrderView
 from .middlewares import LoginCheckMiddleware,LogoutCheckMiddleware
 
 urlpatterns = [
-    path('home',Home.as_view(), name='home'),  
-    path('signup',LogoutCheckMiddleware(Signup.as_view()), name='signup'),
-    path('login',LogoutCheckMiddleware(Login.as_view()), name='login'),
-    path('logout',LoginCheckMiddleware(logout), name='logout'),
-    path('cart',Cart.as_view(), name='cart'),
-    path('checkout',LoginCheckMiddleware(Checkout.as_view()), name='checkout'),
-    path('order',LoginCheckMiddleware(OrderView.as_view()), name='order'),
+    path('home/',Home.as_view(), name='home'),  
+    path('signup/',LogoutCheckMiddleware(Signup.as_view()), name='signup'),
+    path('login/',LogoutCheckMiddleware(Login.as_view()), name='login'),
+    path('logout/',LoginCheckMiddleware(logout), name='logout'),
+    path('cart/',Cart.as_view(), name='cart'),
+    path('checkout/',LoginCheckMiddleware(Checkout.as_view()), name='checkout'),
+    path('order/',LoginCheckMiddleware(OrderView.as_view()), name='order'),
 ]
 
 ##เป็นการสร้าง path เพื่อไปสู่หน้าต่างๆ
